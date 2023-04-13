c91f1c5f4fb9db8b19ce415512eaa9d65d215f28
Lera-arr <lerok-mazurenko@yandex.ru>
2023-04-13 19:57:38 +0300

    реализовали нажатие на ссылку product/addProduct

--- src/main/java/com/example/finalapp/controllers/AdminController.java
+++ src/main/java/com/example/finalapp/controllers/AdminController.java
@@ -1,12 +1,20 @@
 package com.example.finalapp.controllers;
 
 import com.example.finalapp.models.Product;
+import com.example.finalapp.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    private final CategoryRepository categoryRepository;
+
+
+    public AdminController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
@@ -15,6 +23,7 @@ public class AdminController {
     @GetMapping("admin/product/add")
     public String addProduct(Model model){
         model.addAttribute("product",new Product());
-        model.addAttribute("category");
+        model.addAttribute("category", categoryRepository.findAll());
+        return "product/addProduct";
     }
 }
--- src/main/java/com/example/finalapp/repositories/CategoryRepository.java
+++ src/main/java/com/example/finalapp/repositories/CategoryRepository.java
@@ -1,11 +1,10 @@
 package com.example.finalapp.repositories;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Category extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<CategoryRepository, Integer> {
     com.example.finalapp.models.Category findByName(String name);
 }
 //Нахождение бд в нужной категории
