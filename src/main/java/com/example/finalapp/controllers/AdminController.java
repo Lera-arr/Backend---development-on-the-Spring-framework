75e00546e358b62fade11ce1e1eb9b7bfd8b1ab8
Lera-arr <lerok-mazurenko@yandex.ru>
2023-04-13 22:10:28 +0300

    Создали ProductRepository 1.04

--- src/main/java/com/example/finalapp/controllers/AdminController.java
+++ src/main/java/com/example/finalapp/controllers/AdminController.java
@@ -1,19 +1,26 @@
 package com.example.finalapp.controllers;
 
 import com.example.finalapp.models.Product;
+import com.example.finalapp.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+
     @GetMapping("/admin")
     public String admin(){
         return "admin";
+    }
+    private final CategoryRepository categoryRepository;
+    public AdminController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
     @GetMapping("admin/product/add")
     public String addProduct(Model model){
         model.addAttribute("product", new Product());
         model.addAttribute("category");
-        return "product/addProduct";
+        return "product/addProduct";}
 }
