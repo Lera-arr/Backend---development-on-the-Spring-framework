88329be6e8a16adeca1f34b1b5d3d4ab0f291039
Lera-arr <lerok-mazurenko@yandex.ru>
2023-04-13 23:41:51 +0300

    Добавила гетер и сетер Category @PostMapping("/admin/product/add") часть. Категория невидна

--- src/main/java/com/example/finalapp/controllers/AdminController.java
+++ src/main/java/com/example/finalapp/controllers/AdminController.java
@@ -3,7 +3,6 @@ package com.example.finalapp.controllers;
 import com.example.finalapp.models.Category;
 import com.example.finalapp.models.Product;
 import com.example.finalapp.repositories.CategoryRepository;
-import com.example.finalapp.security.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -15,36 +14,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @Controller
 public class AdminController {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    private final CategoryRepository categoryRepository;
+    public AdminController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     @GetMapping("/admin")
     public String admin(){
         return "admin";
     }
 
-
-    @Value("${upload.path}")
-    private String uploadPath;
-
-    private final CategoryRepository categoryRepository;
-
-    public AdminController(ProductService productService, CategoryRepository categoryRepository) {
-
-        this.categoryRepository = categoryRepository;
-    }
-
-
-
-
-    @GetMapping("admin/product/add")
+    @GetMapping("/admin/product/add")
     public String addProduct(Model model){
         model.addAttribute("product", new Product());
         model.addAttribute("category");
-        return "product/addProduct";}
+        return "product/addProduct";
+    }
 
-
+    @PostMapping("/admin/product/add")
+    public void addProduct(@ModelAttribute("product") @Valid Product product, BindingResult bindingResult,
+                              @RequestParam("file_one")MultipartFile file_one, @RequestParam("file_two")MultipartFile file_two, @RequestParam("file_three")MultipartFile file_three,@RequestParam("file_four")MultipartFile file_four,@RequestParam("file_five")MultipartFile file_five,@RequestParam("category")int category, Model model){
+        Category category_db = (Category) categoryRepository.findById(category).orElseThrow();
+        System.out.println(category_db.getName());
+    }
 
 }
--- src/main/java/com/example/finalapp/models/Category.java
+++ src/main/java/com/example/finalapp/models/Category.java
@@ -11,6 +11,31 @@ public class Category {
     private int id;
 
     private String name;
+
     @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
     private List<Product> product;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
+    }
 }
